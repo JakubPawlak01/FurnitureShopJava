56d322304668a86748a16bfff77b00995f098f2f
Jakub <jakubpawlak2001@gmail.com>
2023-04-11 10:47:44 +0200

    123

--- Client.java
+++ Client.java
@@ -52,8 +52,32 @@ public void callMenu1() {
 }
 
 public void callMenu2() {
+    printer.printChooseFurniture();
+
     String type = printer.getTypeFromUser();
+
+    if(type.equals("Stół")) {
+        printer.printChooseTable();
+    }
+    else if(type.equals("Krzesło")) {
+        printer.printChooseChair();
+    }
+    else if(type.equals("Łóżko")) {
+        printer.printChooseBed();
+    }
+    else if(type.equals("Szafa")) {
+        printer.printChooseWardobe();
+    }
+    else if(type.equals("Komoda")) {
+        printer.printChooseDresser();
+    }
+    else {
+        printer.printBadChoice();
+    }
+
     String model = printer.getModelFromUser();
+
+    printer.printChooseColor();
     String color = printer.getColorFromUser();
     int price = printer.getPriceFromUser();
     int id = printer.getIdFromUser();
